ca7871666e4592367299fe44102d09c0a2ab0dd0
Laitch <luizhenriquebfdossantos@gmail.com>
2025-10-03 11:22:40 -0300

    Update niveldeacesso_funcionarios.py

--- niveldeacesso_funcionarios.py
+++ niveldeacesso_funcionarios.py
@@ -1,47 +1,55 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 import mysql.connector
-import io
+from tabela_produtos import Produtos
+from tabela_alunos import Alunos
  
-def Funcionario():
-    conexao = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="escola",
-        port=3307
-    )
+def NiveldeAcessoFuncionarios():
+ conexao = mysql.connector.connect(
+ host="localhost",
+ user="root",
+ password="", 
+ database="escola",
+ port="3307"
  
-    janela = tk.Tk()
-    janela.title("Tabela de Funcionários")
-    janela.geometry("1200x800")
+)
+ cursor = conexao.cursor()
  
-    cursor = conexao.cursor()
-    cursor.execute("SELECT id_Funcionario, Nome, email, senha, Foto, Turno FROM trabalhadores")
-    funcionarios = cursor.fetchall()
+ query = "SELECT Direitos FROM nivel_de_acesso WHERE ChaveDeAcesso = %s"
+ cursor.execute(query, (2,))
  
-    headers = ["ID", "Nome", "Email", "Senha", "Foto", "Turno"]
-    for col, texto in enumerate(headers):
-        tk.Label(janela, text=texto, font="Arial 10 bold", bg="#dfe6e9").grid(row=1, column=col, padx=10, pady=5)
+ resultado = cursor.fetchone()
  
-    imagens = []  
+ cursor.close()
+ conexao.close()
  
-    for i, funcionario in enumerate(funcionarios, start=2):
-        for j, valor in enumerate(funcionario):
-            if j == 4 and valor: 
-                imagem_binaria = io.BytesIO(valor)
-                imagem = Image.open(imagem_binaria)
-                imagem = imagem.resize((200, 200))
-                imagem_tk = ImageTk.PhotoImage(imagem)
-                imagens.append(imagem_tk)
-                tk.Label(janela, image=imagem_tk).grid(row=i, column=j, padx=10, pady=2)
-            else:
-                tk.Label(janela, text=str(valor), bg="#f1f2f6").grid(row=i, column=j, padx=10, pady=2)
+ janela = tk.Tk()
+ janela.title("Nível de Acesso")
+ janela.resizable = True
+ janela.geometry("1000x800")
  
-    cursor.close()
-    conexao.close()
-    janela.mainloop()
+ def clique_produtos():
+ janela.destroy()
+ Produtos()
+ 
+ def clique_alunos():
+ janela.destroy()
+ Alunos()
+ 
+ label_nome = tk.Label(janela, text="Nível de Acesso: Funcionário", font=("Arial", 24))
+ imagem = tk.PhotoImage(file="C:\\Users\\MOBSP-041\\Downloads\\Design sem nome (1).png")
+ label_imagem = tk.Label(janela, image=imagem)
+ label_descricao = tk.Label(janela, text=resultado, font=("Arial", 18))
+ botao_acessar_alunos = tk.Button(janela, text="Acessar tabela de alunos", command=clique_alunos, height=3, width=40)
+ botao_acessar_produtos = tk.Button(janela, text="Acessar Produtos", command=clique_produtos, height=3, width=40)
+ 
+ label_nome.pack(pady=(100,0))
+ label_descricao.pack()
+ label_imagem.pack(pady=(50,0))
+ botao_acessar_alunos.pack(pady=(40,0))
+ botao_acessar_produtos.pack(pady=(10,0))
+ janela.mainloop()
  
 if __name__ == "__main__":
-    Funcionario()
+ NiveldeAcessoFuncionarios()
+ 
  
